c9af0b9c6cb606eed9b64604414805c8cf05f18d
rishikumar-effi <rishikumar.k@efficienza.in>
2025-06-13 09:53:31 +0530

    changed step components import to lazy

--- src/components/StepperComponent.tsx
+++ src/components/StepperComponent.tsx
@@ -1,6 +1,14 @@
-import { Box, Zoom } from "@mui/material";
+import { lazy, Suspense } from "react";
+import { Box, Typography, Zoom } from "@mui/material";
 import { useSteps } from "../hooks/useSteps";
-import { StepOne, StepTwo, StepThree, StepFour } from "./steps";
+
+const StepOne = lazy(() => import('./steps/StepOne').then(module => ({ default: module.StepOne })));
+
+const StepTwo = lazy(() => import('./steps/StepTwo').then(module => ({ default: module.StepTwo })));
+
+const StepThree = lazy(() => import('./steps/StepThree').then(module => ({ default: module.StepThree })));
+
+const StepFour = lazy(() => import('./steps/StepFour').then(module => ({ default: module.StepFour })));
 
 export const StepperComponent = () => {
     const { activeStep } = useSteps();
@@ -9,7 +17,9 @@ export const StepperComponent = () => {
 
     return <Zoom in={true} key={activeStep} style={{ transitionDelay: "300ms" }} >
         <Box sx={{ display: 'grid', placeItems: 'center', minHeight: '20em' }}>
-            {stepperComponentsMap[activeStep]}
+            <Suspense fallback={<Typography>Loading...</Typography>}>
+                {stepperComponentsMap[activeStep]}
+            </Suspense>
         </Box>
     </Zoom>
 }
